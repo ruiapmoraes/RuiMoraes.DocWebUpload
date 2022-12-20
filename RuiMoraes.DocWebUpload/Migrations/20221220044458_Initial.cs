aa1d68592c7fd17d209f38b53abce79935fbacd1
Rui Moraes <rui-moraes@live.com>
2022-12-20 01:47:02 -0300

    #01 - Criando o banco de dados

---
+++ RuiMoraes.DocWebUpload/Migrations/20221220044458_Initial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RuiMoraes.DocWebUpload.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Documentos",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeDocumento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Caminho = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusDoc = table.Column<int>(type: "int", nullable: false),
+                    DataCriado = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documentos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Documentos",
+                schema: "dbo");
+        }
+    }
+}
